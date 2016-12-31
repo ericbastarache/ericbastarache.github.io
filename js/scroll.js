d3ab1eb95205c542a4d0ee4f6cd891997e097c6a
ericb <eric_bastarache@hotmail.com>
2016-12-31 02:16:34 -0500

    added smooth scrolling

--- js/scroll.js
+++ js/scroll.js
@@ -1,5 +1,4 @@
 $(document).on("scroll", onScroll);
-
 //Smooth scrolling
 $("a[href*=#]:not([href=#])").click(function() {
     if (location.pathname.replace(/^\//,"") == this.pathname.replace(/^\//,"") && location.hostname == this.hostname) {
@@ -7,9 +6,23 @@ $("a[href*=#]:not([href=#])").click(function() {
       target = target.length ? target : $("[name=" + this.hash.slice(1) +"]");
       if (target.length) {
         $("html,body").animate({
-          scrollTop: target.offset().top - 80
+          scrollTop: target.offset().top - 90
         }, 1000);
         return false;
       }
     }
 });
+
+function onScroll (event) {
+	var scrollPos = $(document).scrollTop();
+
+	$("#navbar a").each(function () {
+		var currLink = $(this);
+		var refElement = $(currLink.attr("href"));
+		if(refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+			currLink.addClass("active");
+		} else {
+			currLink.removeClass("active");
+		}
+	});
+}
